df3674e7173b2c22668b4d67f6821ec16ec49205
Gabriel <gnunes1909@gmail.com>
2022-08-11 23:45:12 -0300

    Criando new url com OkHttpClient e classe provedora de md5 hash

--- app/src/main/java/com/example/marvelapp/view/util/constants/Constants.kt
+++ app/src/main/java/com/example/marvelapp/view/util/constants/Constants.kt
@@ -5,4 +5,9 @@ object Constants {
     const val BASE_URL = "http://gateway.marvel.com/v1/public/"
     const val PUBLIC_KEY = "895d77fa4fdefd458d1b1344767cfa30"
     const val PRIVATE_KEY = "23780cc6fee988ddc55710ea293f3ca70ed3e506"
+
+    const val API_KEY = "apikey"
+    const val TS = "ts"
+    const val HASH = "hash"
+
 }
